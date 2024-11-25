98f82fe12840ee6bf52168d6a562aab44f9c0176
SI-tanbir <si017490@gmail.com>
2024-11-25 10:10:47 +0600

    adding fire base

---
+++ src/components/firbase_config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGA1WZiXxmvhMwydRmS2h5qWx3JvZ3S80",
+  authDomain: "fir-project-85b45.firebaseapp.com",
+  projectId: "fir-project-85b45",
+  storageBucket: "fir-project-85b45.firebasestorage.app",
+  messagingSenderId: "522417789952",
+  appId: "1:522417789952:web:6b98ef9d5af79218e08ce5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
